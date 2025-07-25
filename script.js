4acd0e14bc39929abac6f12b6d6f7eb24f7665f3
Seth Ferreira <semfware@gmail.com>
2025-07-25 16:47:27 -0400

    Add control flow

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
 const registeredEarly = true;
-const runnerAge = 19;
+const runnerAge = 18;
 
-if (runnerAge > 18 && registeredEarly === true) {
+if (runnerAge > 18 && registeredEarly === false) {
     raceNumber += 1000;
 }
 if (runnerAge > 18 && registeredEarly === true) {
-    console.log(`Runner #${raceNumber} 9:30am`)
+    console.log(`Runner #${raceNumber} 9:30am`);
+} else if (runnerAge > 18 && registeredEarly === false) {
+    console.log(`Runner #${raceNumber} 11:00am`);
+} else if (runnerAge < 18) {
+    console.log(`Runner #${raceNumber} 12:30pm`);
+} else {
+    console.log('See registration desk');
 }
